63fbe0308249ba5a81a813d593b29dc3f79ba92a
chipsto <chipstoh@gmail.com>
2024-01-30 08:12:34 +0700

    change library to restassured

--- lib/src/test/java/tugasjayjay/tests/ReqResApiTest.java
+++ lib/src/test/java/tugasjayjay/tests/ReqResApiTest.java
@@ -1,59 +1,37 @@
 package tugasjayjay.tests;
-
-import org.junit.Test;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import tugasjayjay.request.ReqResApiClient;
-
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
-
 import java.io.IOException;
 public class ReqResApiTest {
-     private final ReqResApiClient reqResApiClient = new ReqResApiClient();
+    private static final ReqResApiClient reqResApiClient = new ReqResApiClient();
 
-    @Test
-    public void testCreateUserPositiveCase() throws IOException {
-        String response = reqResApiClient.createUser("John Doe", "Software Engineer");
-        assertEquals("HTTP/1.1 201 Created", response);
+        @BeforeClass
+    public static void setup() {
+       
+        RestAssured.baseURI = reqResApiClient.getBaseUrl();
     }
+     @Test
+     public void testGetUserByIdPositiveCaseRestAssured() throws IOException{
+         RestAssured.given().when()
+         .get("/users/2")
+         .then().log().all()
+         .assertThat().statusCode(200)
+         .assertThat().body("data.id", Matchers.equalTo(2));
+     }
 
-    @Test
-    public void testGetUserByIdPositiveCase() throws IOException {
-        // Assuming user with ID 2 exists in ReqRes.in
-        String response = reqResApiClient.getUserById(3);
-        assertEquals("HTTP/1.1 200 OK", response);
-    }
+     @Test
+     public void testGetUserByIdNegativeCaseRestAssured() throws IOException{
+         RestAssured.given().when()
+         .get("/users/99999")
+         .then().log().all()
+         .assertThat().statusCode(404);
+     }
 
-    @Test
-    public void testCreateUserNegativeCase() throws IOException {
-        // Sending invalid data, expecting a 400 Bad Request response
-        String response = reqResApiClient.createUser("", "Invalid Job");
-        assertEquals("HTTP/1.1 400 Bad Request", response);
-    }
 
-    @Test
-    public void testGetUserByIdNegativeCase() throws IOException {
-        // Assuming user with ID 999 does not exist
-        String response = reqResApiClient.getUserById(999);
-        assertEquals("HTTP/1.1 404 Not Found", response);
-    }
-
-    @Test
-    public void testCreateUserBoundaryCase() throws IOException {
-        // Sending data with maximum allowed length, expecting a 201 Created response
-        String name = "a".repeat(256); // Assuming maximum length is 255
-        String job = "a".repeat(256);
-        String response = reqResApiClient.createUser(name, job);
-        assertEquals("HTTP/1.1 201 Created", response);
-    }
-
-    @Test
-    public void testGetUserByIdBoundaryCase() throws IOException {
-        // Assuming user with ID 1 exists, testing the minimum and maximum ID values
-        String responseMin = reqResApiClient.getUserById(2);
-        assertEquals("HTTP/1.1 200 OK", responseMin);
-
-        // Assuming user with ID 2147483647 exists (maximum signed 32-bit integer)
-        String responseMax = reqResApiClient.getUserById(2147483643);
-        assertEquals("HTTP/1.1 200 OK", responseMax);
-    }
 }
