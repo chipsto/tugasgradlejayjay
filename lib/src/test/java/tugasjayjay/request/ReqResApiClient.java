20967623a9ade37d7a5d1b8c2f1ca238f78de6ba
chipsto <chipstoh@gmail.com>
2024-01-31 21:55:35 +0700

    update change method tugas 18 modul 20

--- lib/src/test/java/tugasjayjay/request/ReqResApiClient.java
+++ lib/src/test/java/tugasjayjay/request/ReqResApiClient.java
@@ -1,5 +1,5 @@
 package tugasjayjay.request;
-
+import java.io.File;
 
 public class ReqResApiClient {
     private final String baseURL = "https://reqres.in/api";
@@ -7,5 +7,10 @@ public class ReqResApiClient {
 
         return baseURL;
     }
-  
+    
+    public static File jsonSchema(){
+        File jsonSchemaFile = new File("src/test/java/tugasjayjay/Utility/getlistUser.json");
+
+        return jsonSchemaFile;
+    }
 }
--- lib/src/test/java/tugasjayjay/tests/ReqResApiTest.java
+++ lib/src/test/java/tugasjayjay/tests/ReqResApiTest.java
@@ -3,10 +3,14 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import tugasjayjay.request.ReqResApiClient;
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.io.File;
 import java.io.IOException;
 public class ReqResApiTest {
     private static final ReqResApiClient reqResApiClient = new ReqResApiClient();
@@ -18,11 +22,14 @@ public class ReqResApiTest {
     }
      @Test
      public void testGetUserByIdPositiveCaseRestAssured() throws IOException{
+        File jsonschema = reqResApiClient.jsonSchema();
+
          RestAssured.given().when()
-         .get("/users/2")
+         .get("/users?page=2")
          .then().log().all()
          .assertThat().statusCode(200)
-         .assertThat().body("data.id", Matchers.equalTo(2));
+         .assertThat().body("data[0].id", Matchers.equalTo(7))
+         .assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonschema));
      }
 
      @Test
@@ -32,6 +39,27 @@ public class ReqResApiTest {
          .then().log().all()
          .assertThat().statusCode(404);
      }
+
+     @Test
+    public void testGetUserByIdWithMinimumValue() {
+        int userId = 1;  // Minimum value for userId
+
+        RestAssured.given().when()
+                .get("/users/" + userId)
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body("data.id", Matchers.equalTo(userId));
+    }
+
+    @Test
+    public void testGetUserByIdWithMaximumValue() {
+        int userId = Integer.MAX_VALUE;  
+
+        RestAssured.given().when()
+                .get("/users/" + userId)
+                .then().log().all()
+                .assertThat().statusCode(404);  
+    }
 
 
 }
